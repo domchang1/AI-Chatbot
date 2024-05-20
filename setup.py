b3e118f6a2ba6347aec6052cbe67e3b35191f597
domchang1 <dominicchang08@gmail.com>
2024-05-20 11:44:07 -0700

    added gitignore and env

--- setup.py
+++ setup.py
@@ -1,5 +1,28 @@
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
-client = OpenAI()
+load_dotenv()
+api_key = os.getenv('API_KEY')
+client = OpenAI(
+    api_key = api_key,
+)
 
-
+def getResponse(text):
+    response = client.chat.completions.create(
+        model="gpt-3.5",
+        messages=[
+        {
+            "role": "system",
+            "content": "You are a friendly and helpful chatbot."
+        },
+        {
+            "role": "user",
+            "content": text
+        }
+        ],
+        temperature=0.5,
+        max_tokens=64,
+        top_p=1
+    )
+    return response
