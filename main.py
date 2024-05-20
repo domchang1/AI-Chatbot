80f9b5f8a981c8c876ad64bf110bb5954688253d
domchang1 <dominicchang08@gmail.com>
2024-05-20 14:04:26 -0700

    added chat history

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+import datetime
 
 load_dotenv()
 api_key = os.getenv('API_KEY')
@@ -10,18 +11,23 @@ client = OpenAI(
 
 messages = []
 messages.append({"role": "system", "content": "You are a friendly and helpful chatbot."})
+
+history = open("chat_history.txt", "w")
+history.write(datetime.datetime.now().strftime("%c") + "\n \n")
+
 while (True):
     msg = input("Message Chatbot: ")
-    if "exit" in msg:
+    history.write("User Input: " + msg + "\n")
+    if "exit()" in msg:
         break
     messages.append({"role": "user", "content": msg})
     response = client.chat.completions.create(
-        model="gpt-3.5",
-        messages=messages,
-        stream=True
-    )
-    for chunk in response:
-        print(chunk.choices[0].delta)
-    messages.append({"role": "assistant", "content": response["choices"][0]["message"]["content"]})
-    # for chunk in response:
-    #     print(chunk.choices[0].delta.content or "", end="")
+        # model="gpt-4o",
+        model ="gpt-3.5-turbo",
+        messages=messages
+    ).choices[0].message.content
+    print(response)
+    history.write("Chatbot Response: " + response + "\n \n")
+    messages.append({"role": "assistant", "content": response})
+
+history.close()
